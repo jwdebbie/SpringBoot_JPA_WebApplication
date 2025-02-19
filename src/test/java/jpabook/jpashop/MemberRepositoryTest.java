47e36789a8105c77729a211683b44a0f12ef1108
jwdebbie <zwn992@sookmyung.ac.kr>
2025-02-19 14:50:24 +0900

    Feat : MemberRepositoryTest 주석 추가

--- src/test/java/jpabook/jpashop/MemberRepositoryTest.java
+++ src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -7,6 +7,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -15,6 +16,7 @@ import static org.junit.Assert.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+// @Rollback(false) 넣을 시 디비에 결과가 나타남
 
 public class MemberRepositoryTest {
 
