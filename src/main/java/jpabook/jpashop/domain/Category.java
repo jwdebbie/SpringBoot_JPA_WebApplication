518f2bc3451be0711b7e99989b3162a28632d6b6
jwdebbie <zwn992@sookmyung.ac.kr>
2025-02-19 21:58:42 +0900

    Feat : Category 파일 생성

---
+++ src/main/java/jpabook/jpashop/domain/Category.java
@@ -0,0 +1,23 @@
+package jpabook.jpashop.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter @Setter
+public class Category {
+
+    @Id @GeneratedValue
+    @Column(name = "category_id")
+    private Long id;
+
+    private String name;
+
+    @ManyToMany
+    private List<item> items = new ArrayList<>();
+
+}
