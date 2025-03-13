f978ed0941fc869f1ef92884b6ecf903b9b535e0
jwdebbie <leejoowon09@gmail.com>
2025-03-13 21:36:03 +0900

    Test : 주문취소 테스트 완료

--- src/test/java/jpabook/jpashop/service/OrderServiceTest.java
+++ src/test/java/jpabook/jpashop/service/OrderServiceTest.java
@@ -67,12 +67,20 @@ public class OrderServiceTest {
     @Test
     public void 주문취소() throws Exception {
         // given
+        Member member = createMember();
+        Book item = createBook("시골 JPA", 10000, 10);
 
+        int orderCount = 2;
+        Long orderId = orderService.order(member.getId(), item.getId(), orderCount);
 
         // when
-
+        orderService.cancelOrder(orderId);
 
         // then
+        Order getOrder = orderRepository.findOne(orderId);
+
+        Assert.assertEquals("주문 취소 시 상태는 CANCEL이다", OrderStatus.CANCEL, getOrder.getStatus());
+        Assert.assertEquals("주문이 취소된 상품은 그만큼 재고가 증가해야 한다", 10, item.getStockQuantity());
     }
 
     private Book createBook(String name, int price, int stockQuantity) {
