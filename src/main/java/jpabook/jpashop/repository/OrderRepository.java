773fa71fb0dbfd7207f11f972d90c6e22858492e
jwdebbie <leejoowon09@gmail.com>
2025-03-13 17:08:20 +0900

    Feat : OrderService 추가

--- src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -1,4 +1,25 @@
 package jpabook.jpashop.repository;
 
+import jakarta.persistence.EntityManager;
+import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderItem;
+import lombok.RequiredArgsConstructor;
+import org.aspectj.weaver.ast.Or;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
 public class OrderRepository {
+
+    private final EntityManager em;
+
+    public void save(Order order) {
+        em.persist(order);
+    }
+
+    public Order findOne(Long id) {
+        return em.find(Order.class, id);
+    }
+
+   // public List<Order> findAll(OrderSearch orderSearch) {}
 }
