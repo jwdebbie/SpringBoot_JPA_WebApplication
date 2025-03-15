f5de1df18337cb6af84e2e998cab39753b59a528
jwdebbie <leejoowon09@gmail.com>
2025-03-16 01:22:33 +0900

    Feat : MemberController 회원 목록 컨트롤러 추가

--- src/main/java/jpabook/jpashop/controller/MemberForm.java
+++ src/main/java/jpabook/jpashop/controller/MemberForm.java
@@ -8,7 +8,7 @@ import jakarta.validation.constraints.NotEmpty;
 
 @Getter
 @Setter
-public class MemberFrom {
+public class MemberForm {
 
     @NotEmpty(message = "회원 이름은 필수 입니다")
     private String name;
